3d75c799619cf7912aaef3fcb3838ce299f9a30b
Janik <janik.juraj2@gmail.com>
2017-09-24 23:17:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/janik/exercise4youtubeapi/Config.java
@@ -0,0 +1,11 @@
+package com.example.janik.exercise4youtubeapi;
+
+/**
+ * Created by janik on 24.09.2017.
+ */
+
+public final class Config {
+    private Config(){}
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCbwnorwH1LnkZq-qK5RoFIilhsQKQ-LR0";
+}
